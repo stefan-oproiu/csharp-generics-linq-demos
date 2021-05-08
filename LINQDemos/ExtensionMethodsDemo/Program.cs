d4e094b71bdb8d6db403157e15d556b1f9188ede
Stefan Oproiu <stefan.oproiu@amdaris.com>
2021-05-08 19:34:13 +0300

    Initial commit

---
+++ LINQDemos/ExtensionMethodsDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using ExtensionMethodsDemo.Extensions;
+using System;
+
+namespace ExtensionMethodsDemo
+{
+    // Make GetDiscountedPrice an extension method
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product { Id = 1, Name = "Face Mask x50", Price = 20 };
+
+            Console.WriteLine(product.GetDiscountedPrice());
+        }
+    }
+}
